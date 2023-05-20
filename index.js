c68b3cd50918f4919ddde8ffac0886382b66e15f
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-20 14:01:59 +0330

    player movement

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ canvas.height = 576;
 
 const player = new Player({ x: 0, y: 0 });
 
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+
 function animate() {
   window.requestAnimationFrame(animate);
 
@@ -15,6 +24,42 @@ function animate() {
 
   //   Player
   player.update();
+
+  //   movement of player
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.a.pressed) {
+    player.velocity.x = -5;
+  }
 }
 
 animate();
+
+// Events For Moving The Player
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+        
+      keys.d.pressed = true;
+      break;
+    case "a":
+        
+      keys.a.pressed = true;
+      break;
+    case "w":
+      player.velocity.y = -20;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      keys.d.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+  }
+});
--- src/Player.js
+++ src/Player.js
@@ -19,6 +19,7 @@ export default class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     if (this.position.y + this.height + this.velocity.y < canvas.height) {
       this.velocity.y += gravity;
