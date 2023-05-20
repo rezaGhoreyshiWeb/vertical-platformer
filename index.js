90fabaa41de2d70a3c33d829438a7e6768e388ce
rezaGhoreyshiWeb <rezaghoreyshi.contact@gmail.com>
2023-05-20 14:23:06 +0330

    background sprite

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import Player from "./src/Player.js";
+import Sprite from "./src/Sprite.js";
 import { c, canvas } from "./src/canvas.js";
 
 canvas.width = 1024;
 canvas.height = 576;
+
+const scaledCanvas = {
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+};
 
 const player = new Player({ x: 0, y: 0 });
 
@@ -15,12 +21,24 @@ const keys = {
   },
 };
 
+const background = new Sprite({
+  position: { x: 0, y: 0 },
+  imageSrc: "./assets/background.png",
+});
+
 function animate() {
   window.requestAnimationFrame(animate);
 
   //   canvas
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  //   setting the backgroud
+  c.save();
+  c.scale(4, 4);
+  c.translate(0, -background.image.height + scaledCanvas.height);
+  background.update();
+  c.restore();
 
   //   Player
   player.update();
@@ -40,11 +58,9 @@ animate();
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "d":
-        
       keys.d.pressed = true;
       break;
     case "a":
-        
       keys.a.pressed = true;
       break;
     case "w":
---
+++ src/Sprite.js
@@ -0,0 +1,20 @@
+import { c } from "./canvas.js";
+
+export default class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    if (!this.image) {
+        return;
+    }
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+
+  update() {
+    this.draw();
+  }
+}
